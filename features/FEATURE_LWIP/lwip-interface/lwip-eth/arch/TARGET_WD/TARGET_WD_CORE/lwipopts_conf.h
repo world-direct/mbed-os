d649c439f6e7552c5b8a0e991ecd88256bb7a4d9
Patrick Frischmann <patrick.frischmann@world-direct.at>
2017-04-25 15:21:49 +0200

    * modified lwIP config (basically increased buffer sizes) for WD_CORE_G1 target ** CAUTION: values should not be considered final but at least we now avoid errors that result from small buffers

--- features/FEATURE_LWIP/lwip-interface/lwip-eth/arch/TARGET_WD/TARGET_WD_CORE/lwipopts_conf.h
+++ features/FEATURE_LWIP/lwip-interface/lwip-eth/arch/TARGET_WD/TARGET_WD_CORE/lwipopts_conf.h
@@ -19,78 +19,49 @@
 #ifndef LWIPOPTS_CONF_H
 #define LWIPOPTS_CONF_H
 
-#define LWIP_TRANSPORT_ETHERNET       1
+//#define LWIP_STATS                  1
+//#define LWIP_STATS_DISPLAY          1
 
-//#define MEM_SIZE                      (1600 * 16)
+#define LWIP_TRANSPORT_ETHERNET     1
 
-#define ENET_RX_RING_LEN              (16)
-#define ENET_TX_RING_LEN              (8)
-#define ENET_ETH_MAX_FLEN             (1522)	// recommended size for a VLAN frame
-#define ENET_BUFF_ALIGNMENT			  16U		/*!< Ethernet buffer alignment. */
+/**
+ * MEM_SIZE: the size of the heap memory. If the application will send
+ * a lot of data that needs to be copied, this should be set high.
+ */
+#define MEM_SIZE                    (1600 * 32)
 
-#define MEM_SIZE                      (ENET_RX_RING_LEN * (ENET_ETH_MAX_FLEN + ENET_BUFF_ALIGNMENT) + ENET_TX_RING_LEN * ENET_ETH_MAX_FLEN)
+/**
+ * MEMP_NUM_PBUF: the number of memp struct pbufs (used for PBUF_ROM and PBUF_REF).
+ * If the application sends a lot of data out of ROM (or other static memory),
+ * this should be set high.
+ */
+#define MEMP_NUM_PBUF				16
 
+/**
+ * MEMP_NUM_IGMP_GROUP: The number of multicast groups whose network interfaces
+ * can be members at the same time (one per netif - allsystems group -, plus one
+ * per netif membership).
+ * (requires the LWIP_IGMP option)
+ */
+#define MEMP_NUM_IGMP_GROUP         8
 
-/* LwIP Stack Parameters (modified compared to initialization value in opt.h) -*/
-/* Parameters set in STM32CubeMX LwIP Configuration GUI -*/
-///*----- Value in opt.h for LWIP_DHCP: 0 -----*/
-//#define LWIP_DHCP 1
-///*----- Value in opt.h for NO_SYS: 0 -----*/
-//#define NO_SYS 1
-///*----- Value in opt.h for SYS_LIGHTWEIGHT_PROT: 1 -----*/
-//#define SYS_LIGHTWEIGHT_PROT 0
-///*----- Value in opt.h for MEM_ALIGNMENT: 1 -----*/
-//#define MEM_ALIGNMENT 4
-///*----- Default Value for MEMP_NUM_SYS_TIMEOUT: 5 ---*/
-//#define MEMP_NUM_SYS_TIMEOUT 4
-///*----- Value in opt.h for LWIP_ETHERNET: LWIP_ARP || PPPOE_SUPPORT -*/
-//#define LWIP_ETHERNET 1
-///*----- Value in opt.h for LWIP_DNS_SECURE: (LWIP_DNS_SECURE_RAND_XID | LWIP_DNS_SECURE_NO_MULTIPLE_OUTSTANDING | LWIP_DNS_SECURE_RAND_SRC_PORT) -*/
-//#define LWIP_DNS_SECURE 7
-///*----- Default Value for LWIP_TCP: 1 ---*/
-//#define LWIP_TCP 0
-///*----- Default Value for TCP_QUEUE_OOSEQ: 1 ---*/
-//#define TCP_QUEUE_OOSEQ 0
-///*----- Value in opt.h for TCP_SND_QUEUELEN: (4*TCP_SND_BUF + (TCP_MSS - 1))/TCP_MSS -----*/
-//#define TCP_SND_QUEUELEN 9
-///*----- Value in opt.h for TCP_SNDLOWAT: LWIP_MIN(LWIP_MAX(((TCP_SND_BUF)/2), (2 * TCP_MSS) + 1), (TCP_SND_BUF) - 1) -*/
-//#define TCP_SNDLOWAT 1071
-///*----- Value in opt.h for TCP_SNDQUEUELOWAT: LWIP_MAX(TCP_SND_QUEUELEN)/2, 5) -*/
-//#define TCP_SNDQUEUELOWAT 5
-///*----- Value in opt.h for TCP_WND_UPDATE_THRESHOLD: LWIP_MIN(TCP_WND/4, TCP_MSS*4) -----*/
-//#define TCP_WND_UPDATE_THRESHOLD 536
-///*----- Default Value for LWIP_CALLBACK_API: 1 ---*/
-//#define LWIP_CALLBACK_API 0
-///*----- Default Value for LWIP_NETIF_STATUS_CALLBACK: 0 ---*/
-//#define LWIP_NETIF_STATUS_CALLBACK 1
-///*----- Default Value for LWIP_NETIF_LINK_CALLBACK: 0 ---*/
-//#define LWIP_NETIF_LINK_CALLBACK 1
-///*----- Value in opt.h for LWIP_NETCONN: 1 -----*/
-//#define LWIP_NETCONN 0
-///*----- Value in opt.h for LWIP_SOCKET: 1 -----*/
-//#define LWIP_SOCKET 0
-///*----- Value in opt.h for LWIP_STATS: 1 -----*/
-//#define LWIP_STATS 0
-///*----- Value in opt.h for CHECKSUM_GEN_IP: 1 -----*/
-//#define CHECKSUM_GEN_IP 0
-///*----- Value in opt.h for CHECKSUM_GEN_UDP: 1 -----*/
-//#define CHECKSUM_GEN_UDP 0
-///*----- Value in opt.h for CHECKSUM_GEN_TCP: 1 -----*/
-//#define CHECKSUM_GEN_TCP 0
-///*----- Value in opt.h for CHECKSUM_GEN_ICMP: 1 -----*/
-//#define CHECKSUM_GEN_ICMP 0
-///*----- Value in opt.h for CHECKSUM_GEN_ICMP6: 1 -----*/
-//#define CHECKSUM_GEN_ICMP6 0
-///*----- Value in opt.h for CHECKSUM_CHECK_IP: 1 -----*/
-//#define CHECKSUM_CHECK_IP 0
-///*----- Value in opt.h for CHECKSUM_CHECK_UDP: 1 -----*/
-//#define CHECKSUM_CHECK_UDP 0
-///*----- Value in opt.h for CHECKSUM_CHECK_TCP: 1 -----*/
-//#define CHECKSUM_CHECK_TCP 0
-///*----- Value in opt.h for CHECKSUM_CHECK_ICMP: 1 -----*/
-//#define CHECKSUM_CHECK_ICMP 0
-///*----- Value in opt.h for CHECKSUM_CHECK_ICMP6: 1 -----*/
-//#define CHECKSUM_CHECK_ICMP6 0
-///*-----------------------------------------------------------------------------*/ 
+/**
+ * MEMP_NUM_SYS_TIMEOUT: the number of simultaneously active timeouts.
+ * The default number of timeouts is calculated here for all enabled modules.
+ * The formula expects settings to be either '0' or '1'.
+ */
+//#define MEMP_NUM_SYS_TIMEOUT        (LWIP_TCP + IP_REASSEMBLY + LWIP_ARP + (2*LWIP_DHCP) + LWIP_AUTOIP + LWIP_IGMP + LWIP_DNS + (PPP_SUPPORT*6*MEMP_NUM_PPP_PCB) + (LWIP_IPV6 ? (1 + LWIP_IPV6_REASS + LWIP_IPV6_MLD) : 0))
+
+/**
+ * PBUF_POOL_SIZE: the number of buffers in the pbuf pool.
+ */
+#define PBUF_POOL_SIZE				16
+
+/**
+ * MEMP_NUM_TCPIP_MSG_INPKT: the number of struct tcpip_msg, which are used
+ * for incoming packets. 
+ * (only needed if you use tcpip.c)
+ */
+#define MEMP_NUM_TCPIP_MSG_INPKT    16
 
 #endif
