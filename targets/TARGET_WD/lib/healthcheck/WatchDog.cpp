d292bb4732ead6f3c38c0b4ea6fd159d6ca37c25
Simon Pfeifhofer <simon.pfeifhofer@gmail.com>
2017-06-22 08:26:04 +0200

    - add health-check/watchdog functionalities

---
+++ targets/TARGET_WD/lib/healthcheck/WatchDog.cpp
@@ -0,0 +1,38 @@
+/*
+ * WatchDog.cpp
+ *
+ * Copyright (C) 2017 world-direct.at, MIT License
+ *
+ * Created: 21.06.2017
+ * Author:	Simon Pfeifhofer
+ * EMail:	simon.pfeifhofer@world-direct.at
+ *
+ * Description:
+ *		Provides the watch-dog-functionality (IWDG)
+ *
+ */ 
+
+#include "WatchDog.h"
+
+WatchDog::WatchDog(){
+	
+	
+}
+
+
+void WatchDog::Configure(uint32_t prescaler, uint32_t reload) {
+	
+	_handle.Instance = IWDG;
+	_handle.Init.Prescaler = prescaler; // e.g.: IWDG_PRESCALER_256
+	_handle.Init.Reload = reload;		// Range: 0x0000, 0x0FFFU
+	
+	HAL_IWDG_Init(_handle);
+	
+}
+
+
+void WatchDog::Kick() {
+	
+	HAL_IWDG_Refresh(NULL);
+	
+}
---
+++ targets/TARGET_WD/lib/healthcheck/WatchDog.h
@@ -0,0 +1,45 @@
+/*
+ * WatchDog.h
+ *
+ * Copyright (C) 2017 world-direct.at, MIT License
+ *
+ * Created: 21.06.2017
+ * Author:	Simon Pfeifhofer
+ * EMail:	simon.pfeifhofer@world-direct.at
+ *
+ * Description:
+ *		Provides the watch-dog-functionality (IWDG)
+ *
+ */ 
+
+#ifndef WATCHDOG_H_
+#define WATCHDOG_H_
+
+#include "mbed.h"
+
+class WatchDog {
+	
+	private:
+	
+		IWDG_HandleTypeDef _handle;
+	
+	public:
+	
+
+		WatchDog();
+	
+		/* 
+			Configures the Watchdog
+		*/
+		void Configure(uint32_t prescaler, uint32_t reload);
+	
+		/* 
+			Resets the counter to avoid a system-reset
+		*/
+		void Kick();
+	
+};
+
+#endif
+
+
