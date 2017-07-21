3f31a4a7fd3ade478389dbedd590e5b73552845d
Patrick Frischmann <patrick.frischmann@world-direct.at>
2017-07-21 09:04:16 +0200

    Fix serial rx for routingboard; Add snw config for routingboard

---
+++ targets/TARGET_WD/TARGET_WD_ROUTING/TARGET_WD_ROUTINGMAX/TARGET_WD_ROUTINGMAX_G1/snwconf.h
@@ -0,0 +1,19 @@
+/*
+ * snwconf.h
+ *
+ * Created: 20.07.2017 10:22:07
+ *  Author: Patrick.Frischmann
+ */ 
+#ifndef SNWCONF_H_
+#define SNWCONF_H_
+
+#include "PinNames.h"
+
+#define SNWIO_CONF_PIN_RX				Rx0
+#define SNWIO_CONF_PIN_TX				Tx0
+#define SNWIO_CONF_BAUD					115200
+#define SNWIO_CONF_FRAME_PAUSE_CHARS    3
+#define SNWIO_CONF_TX_BUFFER_SIZE		64
+#define SNWIO_CONF_RX_BUFFER_SIZE		64
+
+#endif /* SNWCONF_H_ */
