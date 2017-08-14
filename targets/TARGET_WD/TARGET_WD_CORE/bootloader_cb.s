19de4ca0c87394204e2cb7f3e247be00429b4764
Günter Prossliner <guenter.prossliner@world-direct.at>
2017-08-14 17:08:28 +0200

    restructured bl_hal_init to enable CRC clock, to experiment withit

--- targets/TARGET_WD/TARGET_WD_CORE/bootloader_cb.s
+++ targets/TARGET_WD/TARGET_WD_CORE/bootloader_cb.s
@@ -16,17 +16,27 @@
 .type bl_hal_init, %function
 bl_hal_init:
 
+	/////////////////////////////////////////////////////
+	// CLOCKS:
+	// 
+	// first we need to enable the clocks needed in bootloader in AHB1 bus
+	//	GPIOE (for bus-led): bit #4 (GPIOEEN) on the RCC_AHB1ENR register (@40023830)
+	//  CRC (for validation): bit #12 (CRCEN) also on the RCC_AHB1ENR register (@40023830)
+
+	// set enable bits in RCC_AHB1ENR register (@40023830)
+	// this register has a default value of != 0, so we need to read it
+	LDR r3, =0x40023830	// use r3 for register address
+	LDR r2, [r3]		// read current value into r2 (should be 0x00100000 as documented)
+	LDR r1, =#0x1010	// the mask
+	ORR r2, r2, r1	// perform OR
+	STR r2, [r3, #0]	// and write to register
+
+
 	// turn on BUS_LED to signal start of bootloader
 	// MED will turn it of on initialization (somewhere in software_init_hook), so you will just see it flashing!
 	/////////////////////////////////////////////////////
 
 	// start GPIOE Clk
-	// we need to set bit #4 (GPIOEEN) on the RCC_AHB1ENR register (@40023830)
-	// this register has a default value of != 0, so we need to read it
-	LDR r3, =0x40023830	// use r3 for register address
-	LDR r2, [r3]		// read current value into r2 (should be 0x00100000 as documented)
-	ORR r2, r2, #0x10	// perform OR
-	STR r2, [r3, #0]	// and write to register
 
 	// turn on the BUS-LED (PE10 = 0x4A)
 	// port = 0x4A >> 4 =  0x4 GPIOE_BASE = 0x40021000
