31d303608a27747a72fd13ead098c7ba1061242c
Günter Prossliner <guenter.prossliner@world-direct.at>
2017-10-31 13:59:44 +0100

    fixed the ugly startup issue...

--- targets/TARGET_WD/bl/metadata.s
+++ targets/TARGET_WD/bl/metadata.s
@@ -11,6 +11,3 @@
 	. = WD_ABI_METADATA_FLDOFF_APPLICATION_VERSION;		.asciz EXPSTR(APPLICATION_VERSION)
 	. = WD_ABI_METADATA_FLDOFF_CPUID_MASK;				.word WD_ABI_CPUID_MASK
 	. = WD_ABI_METADATA_FLDOFF_CPUID;					.word WD_ABI_CPUID
-
-.section .crc32,"a",%progbits
-	.word WD_ABI_UNVERIFIABLE_CRC_VALUE		// this will be linked at the end of FLASH, and patched in the elf file to the correct crc value
